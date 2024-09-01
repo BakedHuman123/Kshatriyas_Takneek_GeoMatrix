5ecf9be6d321b6ec175622944a755c5442b36361
Akshat Jain <143754159+AJ-7569@users.noreply.github.com>
2024-09-01 19:02:28 +0530

    Player Tracking

--- Assets/Scripts/PlayerTracking.cs
+++ Assets/Scripts/PlayerTracking.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class PlayerTracking : MonoBehaviour
 {
-    public Transform Player;
+    public GameObject Player;
     public float speed = 5f;
-    //private RigidBody rb;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        //rb = GameObject.GetComponent<RigidBody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = new Vector3(Player.position.x-transform.position.x, 0, Player.position.z-transform.position.z);
-
+        Vector3 direction = new Vector3(Player.transform.position.x-transform.position.x, 0, Player.transform.position.z-transform.position.z);
+        rb.velocity = direction.normalized * speed;
     }
 }
